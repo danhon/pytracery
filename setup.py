0686de9cca39ca81e7079f6a715e427d55993a8b
Dan Hon <dan@danhon.com>
2024-12-15 10:24:13 -0800

    Update setup.py
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=[
         'tracery',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>3.5',
     install_requires=['pyjson5>=1.6.7'],
     license="Apache License 2.0",
     zip_safe=True,
